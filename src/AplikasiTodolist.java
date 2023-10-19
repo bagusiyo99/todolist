62185e9ba79c10834f352d751ec996927ba3ca30
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-19 12:43:34 +0700

    update

---
+++ src/AplikasiTodolist.java
@@ -0,0 +1,24 @@
+public class AplikasiTodolist {
+
+    public static String []  model = new String[10];
+    public static void main(String[] args) {
+
+
+
+    }
+
+    // menampilkan todo list
+    public static void showTodoList(){
+
+    }
+
+    // menambah todo ke list
+    public static void addTodoList(){
+
+    }
+
+    // mengapus todo list
+    public static void removeTodoList(){
+
+    }
+}
